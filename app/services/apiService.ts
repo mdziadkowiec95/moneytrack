7ab2f5916fc00069045d937cff8dd8b85c082ed8
Michal Dziadkowiec <michaldziadkowiec@yahoo.pl>
2024-02-15 18:11:33 +0100

    Fix base path logic

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,4 +1,7 @@
-const baseUrl = process && process?.env?.NODE_ENV ? 'http://localhost:3000' : ''
+const baseUrl =
+  process && process?.env?.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : ''
 
 export const apiService = {
   USER: {
